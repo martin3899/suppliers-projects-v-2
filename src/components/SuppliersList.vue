43319cf8454b405fb8eff2c04e6526060b64bd04
martin3899 <martin.vasseur@le-campus-numerique.fr>
2023-09-28 13:37:07 +0200

    Formattage des dates

--- src/components/SuppliersList.vue
+++ src/components/SuppliersList.vue
@@ -1,6 +1,8 @@
 <script>
 import Supplier from "@/components/Supplier.vue";
 import supplier from "@/components/Supplier.vue";
+import {format} from "timeago.js";
+
 export default {
   computed: {
     supplier() {
@@ -17,13 +19,13 @@ export default {
           id: 1,
           name: "Fournisseur 1 ?",
           status: true,
-          checkedAt: new Date()
+          checkedAt: format(new Date())
         },
         {
           id: 2,
           name: "Fournisseur 2",
           status: false,
-          checkedAt: new Date()
+          checkedAt: format(new Date())
         }
       ]
     }
